7cfb9d4d99467fa0b03818c81a678aaa24f2e3f5
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-03-27 21:35:17 +0200

    init: update forms

--- src/Forms/Forms.tsx
+++ src/Forms/Forms.tsx
@@ -1,77 +1,268 @@
-interface FormsState {
-  checkboxes: string[];
-  text: string;
-  date: string;
-  selectedOption: string;
-  file: string;
-  cards: any[]; // Replace "any" with the type of the "cards" array
-  countries: any[]; // Replace "any" with the type of the "countries" array
-  selectedCountry: string;
-}
+import React from "react";
+import axios from "axios";
+import { Country } from "../types";
 
-class Forms extends React.Component<FormsProps, FormsState> {
-  constructor(props: FormsProps) {
+class Forms extends React.Component {
+  constructor(props: any) {
     super(props);
     this.state = {
+      text: "",
+      date: "",
       checkboxes: [],
+      file: null,
+      selectedOption: "",
+      cards: [],
+      countries: [],
+      selectedCountry: "",
+    };
+    this.handleOptionChange = this.handleOptionChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.handleCountryChange = this.handleCountryChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        this.setState({ countries: response.data });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  handleOptionChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ selectedOption: event.target.value });
+  }
+
+  handleTextChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ text: event.target.value });
+  }
+
+  handleDateChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ date: event.target.value });
+  }
+
+  handleFileChange(event: React.ChangeEvent<HTMLInputElement> ) {
+    if (event.target.files && event.target.files.length) {
+      this.setState({ file: event.target.files[0] });
+    } else {
+      this.setState({ file: null });
+    }
+  }
+
+  handleCheckboxChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    const isChecked = event.target.checked;
+    const { checkboxes } = this.state;
+    if (isChecked) {
+      this.setState({ checkboxes: [...checkboxes, value] });
+    } else {
+      this.setState({
+        checkboxes: checkboxes.filter((item) => item !== value),
+      });
+    }
+  }
+
+  handleCountryChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    this.setState({ selectedCountry: event.target.value });
+  }
+
+  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const {
+      text,
+      date,
+      selectedOption,
+      file,
+      selectedCountry,
+      checkboxes,
+      cards,
+    } = this.state;
+    if (
+      !text ||
+      !date ||
+      !selectedOption ||
+      !file ||
+      !selectedCountry ||
+      checkboxes.length === 0 ||
+      (selectedCountry && !selectedCountry.trim())
+    ) {
+      window.alert("Please fill in all fields.");
+      return;
+    } else {
+      window.alert("Data successfully saved.");
+    }
+
+    const newCard = {
+      text,
+      date,
+      selectedOption,
+      checkboxes,
+      file,
+    };
+
+    this.setState({
+      cards: [...cards, newCard],
       text: "",
       date: "",
       selectedOption: "",
-      file: "",
-      cards: [], // Initialize the "cards" array to an empty array
-      countries: [], // Initialize the "countries" array to an empty array
-      selectedCountry: "",
-    };
+      checkboxes: [],
+      file: null,
+    });
   }
 
-  handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value, checked } = event.target;
-    const { checkboxes } = this.state;
-    this.setState({
-      checkboxes: checked
-        ? [...checkboxes, value]
-        : checkboxes.filter((item: string) => item !== value), // Define the type of the "item" parameter as a string
-    });
-  };
-
-  handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ text: event.target.value });
-  };
-
-  handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ date: event.target.value });
-  };
-
-  handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    this.setState({ selectedOption: event.target.value });
-  };
-
-  handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      this.setState({ file: file.name });
-    }
-  };
-
-  handleCountryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    this.setState({ selectedCountry: event.target.value });
-  };
-
-  handleAddCard = () => {
-    const { text, date, checkboxes, file, selectedOption } = this.state;
-    const newCard = {
+  render() {
+    const {
       text,
       date,
       checkboxes,
       file,
       selectedOption,
-    };
-    this.setState((prevState) => ({
-      cards: [...prevState.cards, newCard],
-      text: "",
-      date: "",
-      checkboxes: [],
-      file: "",
-      selectedOption: "",
-    }));
-  };
+      cards,
+      countries,
+      selectedCountry,
+    } = this.state;
+
+    return (
+      <section>
+      <div className="form">
+        <h1>Fill out your information</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="inputName">
+            <label>
+              Name:
+              <input type="text" value={text} onChange={this.handleTextChange} />
+            </label>
+          </div>
+          <div className="inputDate">
+            <label>
+              Your birthday:
+              <input type="date" value={date} onChange={this.handleDateChange} />
+            </label>
+          </div>
+          <div className="inputSex">
+            <p>Sex:</p>
+            <div className="inputSexLabel">
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    value="Male"
+                    checked={selectedOption === "Male"}
+                    onChange={this.handleOptionChange}
+                  />
+                  Male
+                </label>
+              </div>
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    value="Female"
+                    checked={selectedOption === "Female"}
+                    onChange={this.handleOptionChange}
+                  />
+                  Female
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="selectCountry">
+            <label className="country-select" htmlFor="country-select">
+              Select a country:
+            </label>
+            <select
+              id="country-select"
+              value={selectedCountry}
+              onChange={this.handleCountryChange}
+            >
+              <option value="">Choose a country</option>
+              {countries.map((country) => (
+                <option key={country.name.common} value={country.name.common}>
+                  {country.name.common}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="inputPersonal">
+            <p>I consent to my personal data</p>
+            <label>
+              <input
+                type="checkbox"
+                name="checkbox"
+                value="Yes"
+                defaultChecked={checkboxes.includes("Yes")}
+                onChange={this.handleCheckboxChange}
+              />
+              Yes
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="checkbox"
+                value="No"
+                defaultChecked={checkboxes.includes("No")}
+                onChange={this.handleCheckboxChange}
+              />
+              No
+            </label>
+          </div>
+          <div className="inputFile">
+            <label>
+              Profile photo:
+              <input type="file" onChange={this.handleFileChange} />
+            </label>
+          </div>
+          <div className="submit">
+            <button type="submit">Отправить</button>
+          </div>
+        </form>
+      </div>
+      <div>
+        {cards.length > 0 ? (
+          <div>
+            <h2>Information cards</h2>
+            {cards.map((card, index) => (
+              <div className="card" key={index}>
+                <h3>Card {index + 1}</h3>
+                <div className="card_info">
+                  <p>Name: {card.text}</p>
+                  <p>Your birthday: {card.date}</p>
+                  <p>Sex: {card.selectedOption}</p>
+                  <p>I consent to my personal data: {card.checkboxes}</p>
+                  {selectedCountry && (
+                    <div>
+                      <p>
+                        Country: {selectedCountry ? countries.find(country => country.name.common === selectedCountry)?.name.common : ''}
+                      </p>
+                    </div>
+                  )}
+                  {card.file && (
+                    <div>
+                      <h4>Profile photo:</h4>
+                      <img
+                        src={URL.createObjectURL(card.file)}
+                        alt="profile"
+                        style={{ width: "250px" }}
+                      />
+                    </div>
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>
+            <h2>Information cards</h2>
+            <p>No data to display.</p>
+          </div>
+        )}
+      </div>
+    </section>
+                );
+            }
+          }
+            export default Forms;
