ec0d4c6c154c21d85c844e9c7236a1294775a23a
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-03-28 20:19:22 +0200

    init: update

--- src/__tests__/MyComponent.test.tsx
+++ src/__tests__/MyComponent.test.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen  } from '@testing-library/react';
 import Forms from '../Forms/Forms';
+import '@testing-library/jest-dom';
+
 
 describe('Forms component', () => {
-  it('should render without errors', () => {
-    const { getByTestId } = render(<Forms onSubmit={jest.fn()} card={{}} />);
-    expect(getByTestId('form')).toBeInTheDocument();
-  });
-
-  it('should update state on option change', () => {
-    const { getByLabelText } = render(<Forms onSubmit={jest.fn()} card={{}} />);
-    const radioBtn = getByLabelText('Option 1');
-    fireEvent.change(radioBtn, { target: { value: 'option1' } });
-    expect(radioBtn).toBeChecked();
+  it('should update selectedOption on radio button change', () => {
+    render(<Forms onSubmit={jest.fn()} card={{}} />);
+    const maleRadioBtn = screen.getByLabelText('Male') as HTMLInputElement;
+    const femaleRadioBtn = screen.getByLabelText('Female') as HTMLInputElement;
+    fireEvent.click(femaleRadioBtn);
+    expect(maleRadioBtn.checked).toBeFalsy();
+    expect(femaleRadioBtn.checked).toBeTruthy();
   });
 
   it('should update state on text change', () => {
     const { getByLabelText } = render(<Forms onSubmit={jest.fn()} card={{}} />);
-    const input = getByLabelText('Text Input');
+    const input = getByLabelText('Name:');
     fireEvent.change(input, { target: { value: 'test' } });
     expect(input).toHaveValue('test');
   });
 
   it('should update state on date change', () => {
     const { getByLabelText } = render(<Forms onSubmit={jest.fn()} card={{}} />);
-    const input = getByLabelText('Date Input');
+    const input = getByLabelText('Your birthday:');
     fireEvent.change(input, { target: { value: '2022-01-01' } });
     expect(input).toHaveValue('2022-01-01');
   });
@@ -32,38 +31,8 @@ describe('Forms component', () => {
   it('should update state on file change', () => {
     const { container } = render(<Forms onSubmit={jest.fn()} card={{}} />);
     const input = container.querySelector('input[type="file"]') as HTMLInputElement;
-    fireEvent.change(input, {
-      target: { files: [new File(['(⌐□_□)'], 'test.png', { type: 'image/png' })] },
-    });
-    expect(input.files![0].name).toBe('test.png');
-  });
-
-  it('should update state on checkbox change', () => {
-    const { getByLabelText } = render(<Forms onSubmit={jest.fn()} card={{}} />);
-    const checkbox1 = getByLabelText('Checkbox 1');
-    const checkbox2 = getByLabelText('Checkbox 2');
-    fireEvent.click(checkbox1);
-    fireEvent.click(checkbox2);
-    expect(checkbox1).toBeChecked();
-    expect(checkbox2).toBeChecked();
-  });
-
-  it('should update state on country change', () => {
-    const { getByLabelText } = render(<Forms onSubmit={jest.fn()} card={{}} />);
-    const select = getByLabelText('Country Select');
-    fireEvent.change(select, { target: { value: 'US' } });
-    expect(select).toHaveValue('US');
-  });
-
-  it('should submit form when all fields are filled', () => {
-    const { getByLabelText, getByText } = render(<Forms onSubmit={jest.fn()} card={{}} />);
-    const input = getByLabelText('Text Input');
-    const select = getByLabelText('Country Select');
-    const checkbox1 = getByLabelText('Checkbox 1');
-    const submitBtn = getByText('Submit');
-    fireEvent.change(input, { target: { value: 'test' } });
-    fireEvent.change(select, { target: { value: 'US' } });
-    fireEvent.click(checkbox1);
-    fireEvent.click(submitBtn);
+    const file = new File(['(⌐□_□)'], 'test.png', { type: 'image/png' });
+    fireEvent.change(input, { target: { files: [file] } });
+    expect(input.files?.[0]).toBe(file);
   });
 });
